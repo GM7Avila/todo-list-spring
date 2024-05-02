29fedfd4ee94a5b1dbd3b940aedac8ed1d7b759c
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-05-02 16:09:06 -0300

    Create User and Task repositories

---
+++ src/main/java/com/todo/simple/repositories/TaskRepository.java
@@ -0,0 +1,20 @@
+package com.todo.simple.repositories;
+
+import com.todo.simple.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_Id(Long id);
+
+    // Using Java Persistence Query Language (JPQL) defined in JPA specification
+        // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+        // List<Task> findByUser_Id(@Param("id") Long id);
+
+
+}
---
+++ src/main/java/com/todo/simple/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.todo.simple.repositories;
+
+import com.todo.simple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
